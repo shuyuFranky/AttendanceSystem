02cb9249a17e64ec3177122d8c5b1046e6ef513f
shuyuFranky <shuyu_@pku.edu.cn>
2017-07-28 23:07:01 +0800

    add persional page

--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,33 @@ router.get('/index', function(req, res, next) {
   res.render('index');
 });
 
+//test
+function randomInt(min,max)
+{
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
+function getRandomTimeStamps( min, max ){
+      var return_list = [];
+      
+      var entries =  randomInt( min, max );
+      for ( var i =0; i < entries; i++ ){
+        var day = new Date();
+        
+        //Genrate random
+        var previous_date = randomInt(0,365);
+        day.setDate( day.getDate() - previous_date );
+        
+        return_list.push( day.getTime() );
+      }
+      return return_list;
+}
+
+/** GET for test */
+router.get('/test', function(req, res, next) {
+  var data = getRandomTimeStamps(50, 500); 
+  res.send(JSON.stringify(data));
+  console.log("hello");
+  console.log("hello");
+})
+
 module.exports = router;
