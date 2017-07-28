d24b9010c59d373ebc1c1ca3d88f7b6ae79e5089
shuyuFranky <shuyu_@pku.edu.cn>
2017-07-29 00:45:14 +0800

    add new github style graph

--- routes/index.js
+++ routes/index.js
@@ -11,33 +11,31 @@ router.get('/index', function(req, res, next) {
   res.render('index');
 });
 
-//test
-function randomInt(min,max)
-{
-    return Math.floor(Math.random()*(max-min+1)+min);
-}
-function getRandomTimeStamps( min, max ){
-      var return_list = [];
+// //test
+// function randomInt(min,max)
+// {
+//     return Math.floor(Math.random()*(max-min+1)+min);
+// }
+// function getRandomTimeStamps( min, max ){
+//       var return_list = [];
       
-      var entries =  randomInt( min, max );
-      for ( var i =0; i < entries; i++ ){
-        var day = new Date();
+//       var entries =  randomInt( min, max );
+//       for ( var i =0; i < entries; i++ ){
+//         var day = new Date();
         
-        //Genrate random
-        var previous_date = randomInt(0,365);
-        day.setDate( day.getDate() - previous_date );
+//         //Genrate random
+//         var previous_date = randomInt(0,365);
+//         day.setDate( day.getDate() - previous_date );
         
-        return_list.push( day.getTime() );
-      }
-      return return_list;
-}
+//         return_list.push( day.getTime() );
+//       }
+//       return return_list;
+// }
 
 /** GET for test */
 router.get('/test', function(req, res, next) {
-  var data = getRandomTimeStamps(50, 500); 
-  res.send(JSON.stringify(data));
-  console.log("hello");
-  console.log("hello");
+  var data = require('../public/data/newData');
+  res.render('test', { data: data.daily_activeness });
 })
 
 module.exports = router;
