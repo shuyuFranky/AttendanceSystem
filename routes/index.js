99375436ab3e50eb5504e0f81807e261e557ae82
舒彧 <fatfat@shuyudeMacBook-Pro.local>
2017-07-29 01:04:53 +0800

    add new gitgraph to the persionPage

--- routes/index.js
+++ routes/index.js
@@ -11,26 +11,6 @@ router.get('/index', function(req, res, next) {
   res.render('index');
 });
 
-// //test
-// function randomInt(min,max)
-// {
-//     return Math.floor(Math.random()*(max-min+1)+min);
-// }
-// function getRandomTimeStamps( min, max ){
-//       var return_list = [];
-      
-//       var entries =  randomInt( min, max );
-//       for ( var i =0; i < entries; i++ ){
-//         var day = new Date();
-        
-//         //Genrate random
-//         var previous_date = randomInt(0,365);
-//         day.setDate( day.getDate() - previous_date );
-        
-//         return_list.push( day.getTime() );
-//       }
-//       return return_list;
-// }
 
 /** GET for test */
 router.get('/test', function(req, res, next) {
