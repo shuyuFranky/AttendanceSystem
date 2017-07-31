02935515333eff9f79ef6e1f734d2cd78c6af0a9
shuyuFranky <shuyu_@pku.edu.cn>
2017-08-01 01:02:52 +0800

    mdf quary data from server and add web widget

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('home', { });
 });
 
 /* GET home page. */
 router.get('/index', function(req, res, next) {
-  res.render('index');
+  res.render('home', { });
 });
 
 
